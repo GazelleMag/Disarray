990769a4c873bce82ec8a91b1ae2e147c028cb21
GazelleMag <joao_xxx@hotmail.com>
2022-01-26 19:24:54 +0000

    cleaned some code about my player movement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,17 +11,32 @@ public class PlayerMovement : MonoBehaviour
 
   void Update()
   {
-    float horizontal = Input.GetAxisRaw("Horizontal");
-    float vertical = Input.GetAxisRaw("Vertical");
-    Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+    Move();
+  }
 
-    if (direction.magnitude >= 0.1f)
+  void Move()
+  {
+    Vector3 inputDirection = GetPlayerInputDirection();
+
+    if (inputDirection.magnitude >= 0.1f)
     {
-      float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-      transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
-
-      Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-      characterController.Move(moveDirection.normalized * movementSpeed * Time.deltaTime);
+      Vector3 camDirection = GetPlayerCamDirection(inputDirection);
+      characterController.Move(camDirection.normalized * movementSpeed * Time.deltaTime);
     }
   }
+
+  Vector3 GetPlayerInputDirection()
+  {
+    float horizontal = Input.GetAxisRaw("Horizontal");
+    float vertical = Input.GetAxisRaw("Vertical");
+    return new Vector3(horizontal, 0f, vertical).normalized;
+  }
+
+  Vector3 GetPlayerCamDirection(Vector3 inputDirection)
+  {
+    float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+    transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+    return Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+  }
+
 }
